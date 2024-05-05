3581581ac59f6cee39d16b33566b7a4c7b58f10f
kadeksantika <118152944+kadeksantika@users.noreply.github.com>
2024-05-06 06:07:24 +0700

    routes for posting/add desawisata

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const AkunController = require("../controllers/akun");
+const DesaWisataController = require("../controllers/desawisata");
 const KategoriDestinasiController = require("../controllers/kategoridestinasi");
 const {checkAuth} = require("../middleware/checkAuth");
 const upload = require('../../config/multerConfig');
@@ -8,15 +9,18 @@ const upload = require('../../config/multerConfig');
 
 // Akun
 router.post("/akun/login", AkunController.login );
-router.delete("/akun/logout", AkunController.logout );
-router.get("/akun/token", AkunController.refreshToken );
 // upload('akun')-> upload ke folder Akun
 // single('foto')-> Field yang berisi file
 router.post("/akun/add", upload('akun').single('foto'), AkunController.postAkun);
+router.delete("/akun/logout",checkAuth, AkunController.logout );
 router.get("/akun",checkAuth, AkunController.getAllAkun );
 router.get("/akun/:id_akun",checkAuth, AkunController.getOneAkun );
 router.patch("/akun/:id_akun",checkAuth,upload('akun').single('foto'), AkunController.updateAkun);
 router.delete("/akun/:id_akun",checkAuth, AkunController.deleteAkun );
+
+// Desa wisata
+router.post("/desawisata/add",upload('desawisata').single('gambar'), DesaWisataController.postDesaWisata);
+
 
 // Kategori Destinasi
 router.post("/kategoridestinasi/add", KategoriDestinasiController.postKategoriDestinasi);
