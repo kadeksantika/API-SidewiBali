20dcd1bf50956093d41d4f7b71260e5ea8464e94
AceVonLaura <66860122+AceVonLaura@users.noreply.github.com>
2024-05-20 19:38:15 +0800

    config to AWS

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
-const db = new Sequelize('db_sidewi_provbali','root','',{
-    host: "localhost",
+const db = new Sequelize('db_sidewi_provbali','dbmasteruser','$?9e8SH)E>}21&]ln6}ke}M5<LK^y`X5',{
+    host: "ls-662da47c0dd232d68a24961ebe6a6400a9738f01.cno8wo8uo4fl.ap-southeast-1.rds.amazonaws.com",
     dialect:"mysql"
 });
 
