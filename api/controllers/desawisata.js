50f04dee7ace7f7019605973c9cc6b899f9fd62e
kadeksantika <118152944+kadeksantika@users.noreply.github.com>
2024-05-23 11:21:25 +0800

    Update & Post with Auto SLug

--- api/controllers/desawisata.js
+++ api/controllers/desawisata.js
@@ -1,12 +1,25 @@
 const DesaWisata = require("../models/desawisata");
 const fs = require("fs");
 const path = require("path");
+const slugify = require('slugify');
+const { Op } = require("sequelize")
+
 
 exports.postDesaWisata = async (req, res) => {
   try {
     const { nama, alamat, deskripsi, maps, kategori, kabupaten } = req.body;
     //   cek apakah ada gambar di upload
     const gambar = req.file ? req.file.filename : null;
+
+
+    let slug = slugify(nama, { lower: true, strict: true });
+
+    let uniqueSlug = slug;
+    let count = 1;
+    while (await DesaWisata.findOne({ where: { slug: uniqueSlug } })) {
+      uniqueSlug = `${slug}-${count}`;
+      count++;
+    }
 
     // Validasi enum kategori
     if (
@@ -35,6 +48,7 @@ exports.postDesaWisata = async (req, res) => {
 
     await DesaWisata.create({
       nama: nama,
+      slug: uniqueSlug,
       alamat: alamat,
       deskripsi: deskripsi,
       gambar: gambar,
@@ -73,6 +87,8 @@ exports.updateDesaWisata = async (req, res) => {
     const { nama, alamat, deskripsi, maps, kategori, kabupaten } = req.body;
     const gambar = req.file ? req.file.filename : null;
 
+
+
     // Validasi enum kategori
     if (
       !Object.values(["Rintisan", "Berkembang", "Maju", "Mandiri"]).includes(
@@ -106,9 +122,20 @@ exports.updateDesaWisata = async (req, res) => {
     }
     if (nama) {
       desaWisata.nama = nama;
+
+      let slug = slugify(nama, { lower: true, strict: true });
+
+      let uniqueSlug = slug;
+      let count = 1;
+      while (await DesaWisata.findOne({ where: { slug: uniqueSlug, id: { [Op.ne]: id } } })) {
+        uniqueSlug = `${slug}-${count}`;
+        count++;
+      }
+
+      desaWisata.slug = uniqueSlug;
     }
     if (gambar) {
-     // Cek apakah ada gambar sebelumnya, jika ada maka hapus gambar sebelumnya
+      // Cek apakah ada gambar sebelumnya, jika ada maka hapus gambar sebelumnya
       if (desaWisata.gambar) {
         const imagePath = path.join(
           __dirname,
@@ -160,73 +187,71 @@ exports.updateDesaWisata = async (req, res) => {
     }
 
     return res.status(500).json({
-      message: "Terjadi kesalahan saat memperbarui Desa wisata",
-      error,
+      error: error.message
     });
   }
 };
 
 exports.deleteDesaWisata = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const desawisata = await DesaWisata.findOne({ where: { id: id} });
-
-      if (!desawisata) {
-        return res.status(404).json({ message: "Desa Wisata tidak ditemukan" });
-      }
-  
-      const gambarDesawisataLama = desawisata.gambar;
-
-      const deletedDesawisataCount = await DesaWisata.destroy({
-        where: {
-          id: id,
-        },
-      });
-      if (deletedDesawisataCount > 0) {
-        if (gambarDesawisataLama) {
-            const imagePath = path.join(__dirname, "../../uploads/resource/desawisata", gambarDesawisataLama);
-            if (fs.existsSync(imagePath)) {
-              fs.unlinkSync(imagePath);
-              console.log("Foto Desa berhasil dihapus:", gambarDesawisataLama);
-            } else {
-              console.log("Foto Desa tidak ditemukan:", gambarDesawisataLama);
-            }
-          }
-        return res.status(200).json({ message: "Desa wisata berhasil dihapus" });
-      } else {
-        return res.status(404).json({ message: "Desa wisata tidak ditemukan" });
-      }
-    } catch (error) {
-      console.error("Error :", error);
-      return res
-        .status(500)
-        .json({ message: "Terjadi kesalahan saat menghapus desa wisata", error });
-    }
-  };
-  
-  exports.getOneDesaWisata = async (req, res) => {
+  try {
     const { id } = req.params;
-    try {
-      const desaWisata = await DesaWisata.findOne({ where: { id: id} });
-      if (!desaWisata) {
-        return res.status(404).json({ error: "Desa wisata tidak ditemukan" });
-      }
-      res.json(desaWisata);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-  exports.getAllDesaWisata = async (req, res) => {
-    try {
-      const desaWisataList = await DesaWisata.findAll({
-        attributes: { exclude: [] },
-      });
-      res.json(desaWisataList);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
+    const desawisata = await DesaWisata.findOne({ where: { id: id } });
 
-  
+    if (!desawisata) {
+      return res.status(404).json({ message: "Desa Wisata tidak ditemukan" });
+    }
+
+    const gambarDesawisataLama = desawisata.gambar;
+
+    const deletedDesawisataCount = await DesaWisata.destroy({
+      where: {
+        id: id,
+      },
+    });
+    if (deletedDesawisataCount > 0) {
+      if (gambarDesawisataLama) {
+        const imagePath = path.join(__dirname, "../../uploads/resource/desawisata", gambarDesawisataLama);
+        if (fs.existsSync(imagePath)) {
+          fs.unlinkSync(imagePath);
+          console.log("Foto Desa berhasil dihapus:", gambarDesawisataLama);
+        } else {
+          console.log("Foto Desa tidak ditemukan:", gambarDesawisataLama);
+        }
+      }
+      return res.status(200).json({ message: "Desa wisata berhasil dihapus" });
+    } else {
+      return res.status(404).json({ message: "Desa wisata tidak ditemukan" });
+    }
+  } catch (error) {
+    console.error("Error :", error);
+    return res
+      .status(500)
+      .json({ message: "Terjadi kesalahan saat menghapus desa wisata", error });
+  }
+};
+
+exports.getOneDesaWisata = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const desaWisata = await DesaWisata.findOne({ where: { id: id } });
+    if (!desaWisata) {
+      return res.status(404).json({ error: "Desa wisata tidak ditemukan" });
+    }
+    res.json(desaWisata);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+exports.getAllDesaWisata = async (req, res) => {
+  try {
+    const desaWisataList = await DesaWisata.findAll({
+      attributes: { exclude: [] },
+    });
+    res.json(desaWisataList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
