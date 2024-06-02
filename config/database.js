8f8cded7d4cf2bb0191f35f76634a64f7e2efa9e
Prajna Paramahansa <satriagama1@gmail.com>
2024-06-02 14:21:30 +0800

    feat(config): add missing port value

--- config/database.js
+++ config/database.js
@@ -7,10 +7,12 @@ const password = process.env.DB_PASSWORD;
 const database = process.env.DB_DATABASE;
 const host = process.env.DB_HOST;
 const dialect = process.env.DB_DIALECT;
+const port = process.env.DB_PORT;
 
 const db = new Sequelize(database, username, password, {
   host: host,
   dialect: dialect,
+  port: port
 });
 
 module.exports = db;
