84b279a5ff2080103aa149bf0a3f5a8525314667
Prajna Paramahansa <satriagama1@gmail.com>
2024-06-02 11:51:54 +0800

    feat(config): add env process without .env

--- config/database.js
+++ config/database.js
@@ -1,7 +1,14 @@
 const { Sequelize } = require('sequelize');
-const db = new Sequelize('db_sidewi_provbali','root','',{
-    host: "localhost",
-    dialect:"mysql"
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
+const host = process.env.DB_HOST;
+const dialect = process.env.DB_DIALECT;
+
+const db = new Sequelize(database, username, password, {
+  host,
+  dialect,
 });
 
 module.exports = db;
